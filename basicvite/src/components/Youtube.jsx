319f33f1b639252a5025593ce0793148884c9d8a
Sourabh thapliyal <75442117+Sourabh745@users.noreply.github.com>
2024-12-01 19:47:47 +0530

    custom react library and jsx

--- basicvite/src/components/Youtube.jsx
+++ basicvite/src/components/Youtube.jsx
@@ -1,9 +1,20 @@
 // Filename should be capital
-const Youtube = () => {
+function Youtube () {
+    const username = "Sourabh"
     return (
         <>
-            <h1>This is My Second time here </h1>
+            <h1>This is My Second time here {2+2}</h1>
+            <h1>My name is {if(username === 'Sourabh') ? "yes": "no"}</h1> {/*  {} inside the braces are evaluate in createElement like imp has shown */}
         </>
     )
 }
 export default Youtube;
+
+// imp
+//const aReactElement = React.createElement(
+    //     'a',
+    //     {href: "https://google.com",target: "_blank"},
+    //     'click me to visit google',
+    //      username // this is ok.
+    // exp need to be like these not like {if(username === 'Sourabh') ? "yes": "no"} .
+    // )
