22a56652b53f57e94cea094a560a1c7ed7434c01
Sourabh thapliyal <75442117+Sourabh745@users.noreply.github.com>
2024-12-01 15:02:10 +0530

    Just create a file on component folder run it

---
+++ basicvite/src/App.jsx
@@ -0,0 +1,13 @@
+import Youtube from "./components/Youtube"
+
+function App() {
+
+  return (
+    <>
+      <h1>Sourabh</h1>
+      <Youtube />
+    </>
+  )
+}
+
+export default App
---
+++ basicvite/src/components/Youtube.jsx
@@ -0,0 +1,9 @@
+// Filename should be capital
+const Youtube = () => {
+    return (
+        <>
+            <h1>This is My Second time here </h1>
+        </>
+    )
+}
+export default Youtube;
